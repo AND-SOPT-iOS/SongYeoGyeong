da1dace0d7b5d7e753e113af366549108a3866c4
0gonge <Challengef0802@gmail.com>
2024-11-07 04:53:30 +0900

    start: 초기 커밋

---
+++ SeminarOf35/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeminarOf35
+//
+//  Created by 송여경 on 11/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
