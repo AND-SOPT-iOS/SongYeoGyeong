e9b823a14176596f481a1a889960d02206e829dc
0gonge <Challengef0802@gmail.com>
2024-12-10 01:47:35 +0900

    chore: foldering for SwiftUI

---
+++ seminarSU/seminarSU/seminarSU/seminarSUApp.swift
@@ -0,0 +1,17 @@
+//
+//  seminarSUApp.swift
+//  seminarSU
+//
+//  Created by 송여경 on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct seminarSUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
